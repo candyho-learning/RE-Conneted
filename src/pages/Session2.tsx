ef714e759336742997448724ac3db79ca4ec9376
Candy Ho <candyho.learning@gmail.com>
2024-04-17 17:52:02 +0800

    chat integrated

--- src/pages/Session2.tsx
+++ src/pages/Session2.tsx
@@ -12,6 +12,18 @@ import { useContext, useEffect, useState } from "react";
 import VideoView from "../components/VideoView";
 import { AuthContext } from "../context/authContext";
 import Login from "./Login";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+
+import "stream-chat-react/dist/css/v2/index.css";
 
 //@ts-ignore
 const userCredential = JSON.parse(localStorage.getItem("userCredential"));
@@ -29,12 +41,18 @@ const client = new StreamVideoClient({
   user,
 });
 
+const chatClient = new StreamChat(API_KEY);
+//@ts-ignore
+chatClient.connectUser(user, () => getStreamUserToken(userId));
+
 export default function Session2() {
   const { isLoggedIn } = useContext(AuthContext);
   const [searchParams] = useSearchParams();
   const callId = searchParams.get("id");
   const callType = searchParams.get("type") || "default";
   const [call, setCall] = useState<Call>();
+  //@ts-ignore
+  const [chatChannel, setChatChannel] = useState<Channel>();
   const imageUrl =
     "https://images.unsplash.com/photo-1631679706909-1844bbd07221?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTMyNzk5Mzl8&ixlib=rb-4.0.3&q=85";
 
@@ -42,6 +60,12 @@ export default function Session2() {
     if (callId) {
       const newCall = client.call(callType, callId);
       setCall(newCall);
+      const chatChannel = chatClient.channel("messaging", callId, {
+        // add as many custom fields as you'd like
+        image: "https://www.drupal.org/files/project-images/react.png",
+        name: "Session Chat",
+      });
+      setChatChannel(chatChannel);
     }
   }, []);
 
@@ -61,8 +85,8 @@ export default function Session2() {
         backgroundSize: "cover",
         position: "relative",
       }}
+      className="session-wrapper"
     >
-      <h4>Session2 page placeholder for session {callId}.</h4>
       {client && call && (
         <StreamVideo client={client}>
           <StreamTheme>
@@ -72,6 +96,18 @@ export default function Session2() {
           </StreamTheme>
         </StreamVideo>
       )}
+      <div className="chat-window">
+        <Chat client={chatClient} theme="str-chat__theme-light">
+          <Channel channel={chatChannel}>
+            <Window>
+              <ChannelHeader />
+              <MessageList />
+              <MessageInput />
+            </Window>
+            <Thread />
+          </Channel>
+        </Chat>
+      </div>
     </div>
   );
 }
