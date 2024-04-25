844f68a9fa5ddbcb6337cf84f4f7aab936274e75
Candy Ho <candyho.learning@gmail.com>
2024-04-25 18:14:14 +0800

    installed shadcn and redone login page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
 import Login from "./Login";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { RocketIcon } from "@radix-ui/react-icons";
+
 export default function Dashboard() {
   const { user, isLoggedIn } = useContext(AuthContext);
   if (!isLoggedIn) return <Login context="force" />;
@@ -13,6 +16,13 @@ export default function Dashboard() {
       <a href="/create-session">
         <button>Create a New Session</button>
       </a>
+      <Alert>
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,73 @@ import { AuthContext } from "../context/authContext";
 type LoginProps = {
   context?: string;
 };
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import friendsMainImage from "@/assets/friends.jpg";
+
+export function Dashboard() {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <a
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            <Button variant="outline" className="w-full">
+              Login with Google
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <a href="#" className="underline">
+              Sign up
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <img
+          src={friendsMainImage}
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
+
 export default function Login({ context }: LoginProps) {
   const { login, isLoggedIn, isLoading } = useContext(AuthContext);
   const [email, setEmail] = useState("");
@@ -14,43 +81,80 @@ export default function Login({ context }: LoginProps) {
     setEmail("");
     setPassword("");
   }
+
   return (
-    <div>
-      {isLoading && <p>Loading...</p>}
-      {!isLoading && (
-        <h1>You are now {isLoggedIn ? "logged in" : "logged out"}</h1>
-      )}
-      <h1>
-        {!isLoggedIn &&
-          (context === "normal"
-            ? "Welcome Back"
-            : "Please Log In to Continue...")}
-      </h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
+    <>
+      <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+        <div className="flex items-center justify-center py-12">
+          <div className="mx-auto grid w-[350px] gap-6">
+            <div className="grid gap-2 text-center">
+              <h1 className="text-3xl font-bold">
+                {!isLoggedIn &&
+                  (context === "normal"
+                    ? "Welcome Back"
+                    : "Log In to Continue")}
+                {isLoggedIn && "You are logged in already"}
+              </h1>
+              <p className="text-balance text-muted-foreground">
+                Enter your email below to login to your account
+              </p>
+            </div>
+            <form onSubmit={handleSubmit} className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  placeholder="coworking@re-connected.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="/forgot-password"
+                    className="ml-auto inline-block text-sm underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </form>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline">
+                Sign up
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="hidden bg-muted lg:block">
+          <img
+            src={friendsMainImage}
+            alt="Image"
+            width="1920"
+            height="1080"
+            className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
           />
         </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </div>
-        <button type="submit">Log In</button>
-      </form>
-    </div>
+      </div>
+    </>
   );
 }
