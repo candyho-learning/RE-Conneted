2cfab5f7033a7b5710cafb4d8ee50accc178ad4c
Candy Ho <candyho.learning@gmail.com>
2024-04-30 16:46:30 +0800

    fix: sort session by startTime on the user profile page

--- src/components/SessionCardCarousel.tsx
+++ src/components/SessionCardCarousel.tsx
@@ -40,54 +40,60 @@ export default function SessionCardCarousel({
               This user isn't hosting any sessions for now.
             </h4>
           )}
-          {sessions.map((session) => (
-            <CarouselItem
-              key={session.sessionId}
-              className="md:basis-1/2 lg:basis-1/3"
-            >
-              <div className="p-1">
-                <Card className="w-full h-full">
-                  <CardHeader>
-                    <img src="https://images.unsplash.com/photo-1594026112284-02bb6f3352fe?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTQyMDIyNzV8&ixlib=rb-4.0.3&q=85"></img>
-                    <CardTitle className="text-xl">
-                      {session.sessionName}
-                    </CardTitle>
-                    <CardDescription>
-                      <p>Rest 15m / Deep Work 45m / Free Chat 10m</p>
-                    </CardDescription>
-                  </CardHeader>
+          {sessions
+            .sort((a, b) => {
+              const startTimeA = a.startTime.toDate().getTime();
+              const startTimeB = b.startTime.toDate().getTime();
+              return startTimeB - startTimeA; // Sort in descending order (latest first)
+            })
+            .map((session) => (
+              <CarouselItem
+                key={session.sessionId}
+                className="md:basis-1/2 lg:basis-1/3"
+              >
+                <div className="p-1">
+                  <Card className="w-full h-full">
+                    <CardHeader>
+                      <img src="https://images.unsplash.com/photo-1594026112284-02bb6f3352fe?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTQyMDIyNzV8&ixlib=rb-4.0.3&q=85"></img>
+                      <CardTitle className="text-xl">
+                        {session.sessionName}
+                      </CardTitle>
+                      <CardDescription>
+                        <p>Rest 15m / Deep Work 45m / Free Chat 10m</p>
+                      </CardDescription>
+                    </CardHeader>
 
-                  <CardContent>
-                    <div className="flex items-center">
-                      <CalendarIcon />
-                      <p className="ml-3">
-                        {session.startTime
-                          .toDate()
-                          .toLocaleString("en-US", dateOptions)}
-                      </p>
-                    </div>
+                    <CardContent>
+                      <div className="flex items-center">
+                        <CalendarIcon />
+                        <p className="ml-3">
+                          {session.startTime
+                            .toDate()
+                            .toLocaleString("en-US", dateOptions)}
+                        </p>
+                      </div>
 
-                    <div className="flex items-center">
-                      <ClockIcon />
-                      <p className="ml-3">
-                        {session.startTime
-                          .toDate()
-                          .toLocaleString("en-US", timeOptions)}
-                      </p>
-                    </div>
-                  </CardContent>
+                      <div className="flex items-center">
+                        <ClockIcon />
+                        <p className="ml-3">
+                          {session.startTime
+                            .toDate()
+                            .toLocaleString("en-US", timeOptions)}
+                        </p>
+                      </div>
+                    </CardContent>
 
-                  <CardFooter>
-                    <a
-                      href={`/coworking-session?type=default&id=${session.sessionId}`}
-                    >
-                      <Button>Join</Button>
-                    </a>
-                  </CardFooter>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
+                    <CardFooter>
+                      <a
+                        href={`/coworking-session?type=default&id=${session.sessionId}`}
+                      >
+                        <Button>Join</Button>
+                      </a>
+                    </CardFooter>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
