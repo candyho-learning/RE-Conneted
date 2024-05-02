d97429bae6f6f149d455cdfe0bc8e58ae7f6f71b
Candy Ho <candyho.learning@gmail.com>
2024-05-02 21:38:19 +0800

    fix: removed old signup page elements

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -35,134 +35,87 @@ export default function Signup() {
     navigate("/dashboard");
   }
   return (
-    <>
-      <div className="w-full h-screen bg-brand-mutedblue lg:flex justify-between lg:min-h-[600px] xl:min-h-[800px]">
-        <div className="flex items-center justify-center py-12 px-60">
-          <div className="mx-auto grid w-[350px] gap-6">
-            <div className="grid gap-2 text-center">
-              <h1 className="text-3xl font-bold">Join RE:Connected</h1>
-              <p className="text-balance text-muted-foreground">
-                Work alone, together, anywhere in the world.
-              </p>
-            </div>
-            <form onSubmit={handleSubmit} className="grid gap-4">
-              <div className="flex space-x-2">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">First Name</Label>
-                  <Input
-                    required
-                    type="text"
-                    name="firstName"
-                    onChange={handleFieldChange}
-                    value={signupForm.firstName}
-                    placeholder="First Name"
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Last Name</Label>
-                  <Input
-                    required
-                    type="text"
-                    name="lastName"
-                    onChange={handleFieldChange}
-                    value={signupForm.lastName}
-                    placeholder="Last Name"
-                  />
-                </div>
-              </div>
-
+    <div className="w-full h-screen bg-brand-mutedblue lg:flex justify-between lg:min-h-[600px] xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12 px-60">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Join RE:Connected</h1>
+            <p className="text-balance text-muted-foreground">
+              Work alone, together, anywhere in the world.
+            </p>
+          </div>
+          <form onSubmit={handleSubmit} className="grid gap-4">
+            <div className="flex space-x-2">
               <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
+                <Label htmlFor="email">First Name</Label>
                 <Input
                   required
-                  type="email"
-                  name="email"
+                  type="text"
+                  name="firstName"
                   onChange={handleFieldChange}
-                  value={signupForm.email}
-                  placeholder="hello@re-connected.com"
+                  value={signupForm.firstName}
+                  placeholder="First Name"
                 />
               </div>
               <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                </div>
+                <Label htmlFor="email">Last Name</Label>
                 <Input
                   required
-                  type="password"
-                  name="password"
+                  type="text"
+                  name="lastName"
                   onChange={handleFieldChange}
-                  value={signupForm.password}
-                  placeholder="Pick a secure password"
+                  value={signupForm.lastName}
+                  placeholder="Last Name"
                 />
               </div>
-
-              <Button type="submit" className="w-full">
-                Sign Up (it's free!)
-              </Button>
-            </form>
-            <div className="mt-4 text-center text-sm">
-              Already have an account?{" "}
-              <a href="/login" className="underline">
-                Log In
-              </a>
             </div>
+
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                required
+                type="email"
+                name="email"
+                onChange={handleFieldChange}
+                value={signupForm.email}
+                placeholder="hello@re-connected.com"
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input
+                required
+                type="password"
+                name="password"
+                onChange={handleFieldChange}
+                value={signupForm.password}
+                placeholder="Pick a secure password"
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Sign Up (it's free!)
+            </Button>
+          </form>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <a href="/login" className="underline">
+              Log In
+            </a>
           </div>
         </div>
-        <div className="hidden bg-brand-mutedblue lg:block w-3/5 ">
-          <img
-            src={Friends2}
-            alt="Image"
-            width="1920"
-            height="1080"
-            className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale custom-clip"
-          />
-        </div>
       </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>First Name: </label>
-            <input
-              required
-              type="text"
-              name="firstName"
-              onChange={handleFieldChange}
-              value={signupForm.firstName}
-            />
-          </div>
-          <div>
-            <label>Last Name: </label>
-            <input
-              required
-              type="text"
-              name="lastName"
-              onChange={handleFieldChange}
-              value={signupForm.lastName}
-            />
-          </div>
-          <div>
-            <label>Email: </label>
-            <input
-              required
-              type="email"
-              name="email"
-              onChange={handleFieldChange}
-              value={signupForm.email}
-            />
-          </div>
-          <div>
-            <label>Password: </label>
-            <input
-              required
-              type="password"
-              name="password"
-              onChange={handleFieldChange}
-              value={signupForm.password}
-            />
-          </div>
-          <button type="submit">Sign Up</button>
-        </form>
+      <div className="hidden bg-brand-mutedblue lg:block w-3/5 ">
+        <img
+          src={Friends2}
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale custom-clip"
+        />
       </div>
-    </>
+    </div>
   );
 }
