16c32186c9af6ee2566996dcd4611112f28eb6e9
Candy Ho <candyho.learning@gmail.com>
2024-05-21 15:40:00 +0800

    added loading state to protected route

--- src/pages/ProtectedRoutes.tsx
+++ src/pages/ProtectedRoutes.tsx
@@ -2,10 +2,12 @@ import { AuthContext } from "@/contexts/authContext";
 import { useContext } from "react";
 import App from "@/App";
 import { Navigate, useLocation } from "react-router-dom";
+import Loading from "@/components/Loading";
 
 export default function ProtectedRoutes() {
-  const { isLoggedIn } = useContext(AuthContext);
+  const { isLoggedIn, isLoading } = useContext(AuthContext);
   const location = useLocation();
+  if (isLoading) return <Loading />;
   return isLoggedIn ? (
     <App />
   ) : (
